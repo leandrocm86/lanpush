d3567e7a69bdaad0b83f19fdba73613c3ab3a64e
Leandro Medeiros <leandro.medeiros@serpro.gov.br>
2023-06-12 17:03:42 -0300

    Finishing the About window

--- src/main/java/lcm/lanpush/AboutWindow.java
+++ src/main/java/lcm/lanpush/AboutWindow.java
@@ -1,45 +1,82 @@
 package lcm.lanpush;
 
+import java.awt.Desktop;
+import java.awt.Dimension;
+import java.io.IOException;
+import java.net.URISyntaxException;
 import java.util.Arrays;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JEditorPane;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.ScrollPaneConstants;
+import javax.swing.SwingConstants;
+import javax.swing.event.HyperlinkEvent;
 
+import lcm.java.swing.Images;
 import lcm.java.swing.Layouts;
+import lcm.java.swing.Screen;
 import lcm.java.swing.SwingComponents;
+import lcm.java.system.logging.OLog;
 
 public class AboutWindow {
 
 private static final String DESCRIPTION =
         """
             <html>
-                THIS IS STILL A WORK IN PROGRESS THIS IS STILL A WORK IN PROGRESS THIS IS STILL A WORK IN PROGRESS.
-                <a href='https://www.google.com'>THIS IS STILL A WORK IN PROGRESS</a> THIS IS STILL A WORK IN PROGRESSTHIS IS STILL A WORK IN PROGRESS.
-                THIS IS STILL A WORK IN PROGRESSTHIS IS STILL A WORK IN PROGRESSTHIS IS STILL A WORK IN PROGRESS.
+                LANPUSH aims to facilitate exchanging text content between devices in the same local network.
+                There's an <a href='https://play.google.com/store/apps/details?id=lcm.lanpush'>Android app</a> on the play store. You can check its <a href='https://github.com/leandrocm86/lanpush-android'>github repository</a> for more details.
+                There's also a simple CLI version (for terminal usage) available as snap, flatpak and JAR. <a href='https://github.com/leandrocm86/lanpush-cli'>Check its repository</a> for details.
+                Finally, this GUI (desktop) version is available for Windows and Linux (snap and flatpak). <a href='https://github.com/leandrocm86/lanpush'>Its repository is also on github</a>.
+                All projects are free, open sourced, have no ads, and are open to suggestions.
             </html>
         """;
-
+    
     final JFrame aboutFrame;
     final JPanel aboutPane;
     final JLabel appIcon;
-    final JLabel descriptionText;
+    final JEditorPane descriptionText;
     final JButton closeButton;
 
     public AboutWindow() {
         aboutFrame = new JFrame("About LANPUSH");
         appIcon = new JLabel();
-        appIcon.setIcon(new ImageIcon(SwingComponents.getImageFromResource("/lanpush.png")));
-        descriptionText = new JLabel(DESCRIPTION);
+        var image = Images.getImageFromResource("/lanpush.png");
+        image = Images.resizeByHeight(image, Config.getProportionalHeight(15));
+        appIcon.setIcon(new ImageIcon(image));
+        appIcon.setHorizontalAlignment(SwingConstants.CENTER);
+        var versionLabel = new JLabel("version: 2.0");
+        var topPane = Layouts.verticalPane(Arrays.asList(appIcon, versionLabel), 7, 3);
+
+        descriptionText = new JEditorPane();
+        descriptionText.setContentType("text/html");
+        descriptionText.setEditable(false);
+        descriptionText.setOpaque(false);
+        descriptionText.putClientProperty(JEditorPane.HONOR_DISPLAY_PROPERTIES, true); // Enable changing font size.
+        descriptionText.setPreferredSize(new Dimension(Config.getWindowWidth(), Config.getProportionalHeight(50)));
+        descriptionText.setText(DESCRIPTION.replace("\n", "<br>"));
+        descriptionText.addHyperlinkListener(event -> {
+            if (event.getEventType() == HyperlinkEvent.EventType.ACTIVATED)
+                try {
+                    Desktop.getDesktop().browse(event.getURL().toURI());
+                } catch (IOException | URISyntaxException e) {
+                    OLog.error(e, "Could not open link on browser: '%s'", event.getURL());
+                }
+        });
+        var scrollPane = SwingComponents.createScrollPane(descriptionText, Config.getProportionalWidth(2));
+        scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+        
         closeButton = new JButton("Close");
         closeButton.addActionListener(e -> aboutFrame.dispose());
-        aboutPane = Layouts.verticalPane(Arrays.asList(appIcon, descriptionText, closeButton), 30, 50, 20);
+        aboutPane = Layouts.fullVerticalPane(Arrays.asList(topPane, scrollPane, closeButton), 25, 65, 10);
         aboutFrame.setContentPane(aboutPane);
 
         Config.getDefaultFont().apply(aboutFrame);
-        aboutFrame.setSize(Config.getProportionalWidth(50), Config.getProportionalHeight(50));
+        aboutFrame.setSize(Config.getProportionalWidth(50), Config.getWindowHeight());
+        Screen.centralizeWindow(aboutFrame);
         aboutFrame.setVisible(true);
         aboutFrame.toFront();
     }
